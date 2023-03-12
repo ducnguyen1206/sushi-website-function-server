2b973b400c1e9594346dd91345896b3e6e117311
ducnguyen1206 <minhduc12037@gmail.com>
2023-03-12 22:41:13 +0700

    Delete comment

--- src/main/java/com/personal/project/application/controller/BookingController.java
+++ src/main/java/com/personal/project/application/controller/BookingController.java
@@ -14,22 +14,11 @@ public class BookingController {
     @Autowired
     private BookingService bookingService;
 
-    /**
-     * Api for booking
-     *
-     * @param bookingRequest the information of the booking
-     * @return the booking information which was saved to the database
-     */
     @PostMapping("/submit")
     public ResponseEntity<Object> submit(@RequestBody BookingDto bookingRequest) {
         return ResponseEntity.ok(bookingService.submit(bookingRequest));
     }
 
-    /**
-     * Api get branch for drop down button
-     *
-     * @return branch list
-     */
     @GetMapping("/branch-list")
     public ResponseEntity<Object> getAllBranches() {
         return ResponseEntity.ok(bookingService.getAllBranches());
